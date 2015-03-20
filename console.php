14b4a0c5594ade84ad0d8b63efc0d1c9214a3b60
Matthieu Napoli <matthieu@mnapoli.fr>
2015-03-20 17:54:08 +1300

    Console improvements

--- console.php
+++ console.php
@@ -1,24 +1,37 @@
 <?php
 
 use PhpAnalyzer\Analyzer;
+use PhpAnalyzer\Parser\Node\ReflectedMethod;
 use Symfony\Component\Console\Output\OutputInterface;
 
 require_once 'vendor/autoload.php';
 
 $app = new Silly\Application;
 
-$app->command('method class method', function ($class, $method, OutputInterface $output) {
+$directories = [
+    __DIR__ . '/src',
+//    __DIR__ . '/vendor/nikic',
+];
+
+$app->command('info class [method]', function ($class, $method, OutputInterface $output) use ($directories) {
     $analyzer = new Analyzer;
-    $scope = $analyzer->analyze([__DIR__ . '/src', __DIR__ . '/vendor/nikic']);
+    $scope = $analyzer->analyze($directories);
 
     $class = str_replace('.', '\\', $class);
     $class = $scope->getClass($class);
-    $method = $class->getMethod($method);
+    if ($method) {
+        $methods = [$class->getMethod($method)];
+    } else {
+        $methods = $class->getMethods();
+    }
 
-    $output->writeln(sprintf('function %s()', $method->getName()));
-    foreach ($method->getScope()->getVariables() as $variable) {
-        $class = str_replace('PhpAnalyzer\Scope\\', '', get_class($variable));
-        $output->writeln(sprintf("\t$%s: %s (%s)", $variable->getName(), $variable->getType()->toString(), $class));
+    foreach ($methods as $method) {
+        /** @var ReflectedMethod $method */
+        $output->writeln(sprintf('function %s()', $method->getName()));
+        foreach ($method->getScope()->getVariables() as $variable) {
+            $class = str_replace('PhpAnalyzer\Scope\\', '', get_class($variable));
+            $output->writeln(sprintf("\t$%s: %s (%s)", $variable->getName(), $variable->getType()->toString(), $class));
+        }
     }
 });
 
