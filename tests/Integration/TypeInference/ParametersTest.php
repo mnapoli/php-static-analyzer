f6bd3d900aaedbb565f3689c414a2b868c202a3a
Matthieu Napoli <matthieu@mnapoli.fr>
2015-05-13 22:28:02 +1200

    Added incomplete tests

--- tests/Integration/TypeInference/ParametersTest.php
+++ tests/Integration/TypeInference/ParametersTest.php
@@ -2,7 +2,6 @@
 
 namespace PhpAnalyzer\Test\Integration\TypeInference;
 
-use PhpAnalyzer\Scope\LocalVariable;
 use PhpAnalyzer\Test\Integration\BaseAnalyzerTest;
 use PhpAnalyzer\Test\Integration\TypeInference\Parameters\ClassWithMethod;
 use PhpAnalyzer\Type\ClassType;
@@ -24,5 +23,54 @@ class ParametersTest extends BaseAnalyzerTest
         $this->assertInstanceOf(ClassType::class, $type);
 
         $this->assertEquals('PhpAnalyzer\Test\Integration\TypeInference\Parameters\SimpleClass', $type->toString());
+    }
+
+    /**
+     * @test
+     */
+    public function should_infer_method_parameter_type_from_default_value()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    /**
+     * @test
+     */
+    public function should_infer_method_parameter_type_from_phpdoc()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    /**
+     * @test
+     */
+    public function should_infer_function_parameter_type_from_type_hint()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    /**
+     * @test
+     */
+    public function should_infer_function_parameter_type_from_default_value()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    /**
+     * @test
+     */
+    public function should_infer_function_parameter_type_from_phpdoc()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    /**
+     * Merge type-hint + default value + phpdoc
+     * @test
+     */
+    public function should_merge_types_inferred_from_different_sources()
+    {
+        $this->markTestIncomplete('TODO');
     }
 }
