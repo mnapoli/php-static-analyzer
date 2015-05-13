5d72a9396e8bfa7d3e511586a27a84c999225e14
Matthieu Napoli <matthieu@mnapoli.fr>
2015-05-13 22:32:50 +1200

    Added incomplete tests

---
+++ tests/Integration/TypeInference/ReturnValuesTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace PhpAnalyzer\Test\Integration\TypeInference;
+
+use PhpAnalyzer\Test\Integration\BaseAnalyzerTest;
+
+/**
+ * Test type inference from function/method return values.
+ */
+class ReturnValuesTest extends BaseAnalyzerTest
+{
+    /**
+     * @test
+     */
+    public function should_infer_method_return_type_from_phpdoc()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    /**
+     * @test
+     */
+    public function should_infer_method_return_type_from_return_statements()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    /**
+     * @test
+     */
+    public function should_infer_function_return_type_from_phpdoc()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    /**
+     * @test
+     */
+    public function should_infer_function_return_type_from_return_statements()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+
+    /**
+     * @test
+     */
+    public function should_merge_types_inferred_from_different_sources()
+    {
+        $this->markTestIncomplete('TODO');
+    }
+}
